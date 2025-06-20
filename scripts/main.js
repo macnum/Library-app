a02e7df59a31238a10005520976b1d2966eca70a
macnum <w.damilola@yahoo.com>
2025-06-20 11:47:10 +0100

    feat: add and style user interface for libary

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 const myLibrary = [];
 
+const btn = document.querySelector('.create-card-btn');
+const form = document.querySelector('.hidden-form');
+const formBtn = document.querySelector('.add-book-btn');
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputPages = document.querySelector('#pages');
+
 function Book(title, author, pages, read) {
 	if (!new.target) {
 		throw Error('No new keyword defined when creating the object');
@@ -7,10 +14,9 @@ function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.read = read;
 }
 Book.prototype.info = function () {
-	return `The ${this.title} by ${this.author}, ${this.pages} pages, read is ${this.read}.`;
+	return `The ${this.title} by ${this.author}, ${this.pages} pages.`;
 };
 
 // const theHobbit = new Book('Hobbit', 'J.R.R. Tolkien', 295, false);
@@ -24,14 +30,31 @@ function addBookToLibrary(title, author, pages, read) {
 	return;
 }
 
-addBookToLibrary('Hobbit', 'J.R.R. Tolkien', 295, false);
-addBookToLibrary('The Hunger Games', 'Suzanne Collins', 384, false);
-
-console.log(myLibrary);
+// addBookToLibrary('Hobbit', 'J.R.R. Tolkien', 295);
+// addBookToLibrary('The Hunger Games', 'Suzanne Collins', 384);
 
 function showBookInLibrary() {
 	myLibrary.forEach((item) => {
-		console.log(item);
+		const div = document.createElement('div');
+		div.classList.add('card');
+	});
+	formBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		const bookTitle = inputTitle.value;
+		const bookAuthor = inputAuthor.value;
+		const bookPages = inputPages.value;
+		addBookToLibrary(bookTitle, bookAuthor, bookPages);
+		console.log(myLibrary);
+		clearInput();
 	});
 }
+function clearInput() {
+	inputTitle.value = ``;
+	inputAuthor.value = ``;
+	inputPages.value = ``;
+}
 showBookInLibrary();
+
+btn.addEventListener('click', (e) => {
+	form.classList.toggle('hidden-form');
+});
